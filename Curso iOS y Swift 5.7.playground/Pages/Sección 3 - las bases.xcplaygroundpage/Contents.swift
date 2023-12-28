384f67ad5e049cd64a9c571ea470f34a25b45519
kontroldev <raulgallego79@icloud.com>
2023-12-28 12:25:53 +0100

    Actualización primeras clases.

---
+++ Curso iOS y Swift 5.7.playground/Pages/Sección 3 - las bases.xcplaygroundpage/Contents.swift
@@ -0,0 +1,34 @@
+import UIKit
+
+
+//CONSTANTES Y VARIABLES
+var str = "Hello, playground"  // 'var' esto es una variable
+str = "Hola Mundo"
+
+let miNombre = "Kontroldev"  // 'let' esto es una constante
+let miFechaDeNacimiento = "02/01/2024"
+
+
+// DECLARACION MULTIPLES
+let miNombre2 = "kontroldev" ,miFechaDeNacimiento2 = "02/01/2024" ,miLugarDeNacimientio2 = "España"  // Esto tambien se puede hacer cuando vas a declarar constantes/variables con una sola linea de codigo.
+
+
+//TIPO DE DATOS
+var x = 18  // Int
+
+var y = 18.8 // Float
+
+
+// OPERADORES ARITMETICOS + - * /
+var sumaEnteros = x + 2
+var sumaFlotantes = y + 2.2
+var suma = Double(x) + y   // aqui debemos deconvertir `x`en tipo Double, ya que y es de tipo Float/Double, y no podemos hacerlo al reves, cambiar y en tipo `Int`por solo cogeria numeros enteros sin decimales.
+var division = Double(x) / 5
+
+
+// ASIGMACION IMPLICITA Y EXPLICITA
+var x2:Int8 = 18  //asigancion explicita
+
+// FLOTANTES
+var y2:Double = 18.8
+
