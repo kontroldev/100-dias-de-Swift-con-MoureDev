1e841b2b443d1a0d7567104a1a08cb3f97656bd0
kontroldev <raulgallego79@icloud.com>
2024-01-02 12:33:47 +0100

    Actualización de clases dia 1 - 02/01/2024

--- Curso iOS y Swift 5.7.playground/Pages/Sección 3 - las bases.xcplaygroundpage/Contents.swift
+++ Curso iOS y Swift 5.7.playground/Pages/Sección 3 - las bases.xcplaygroundpage/Contents.swift
@@ -1,34 +1,36 @@
-import UIKit
+import Foundation
 
 
 //CONSTANTES Y VARIABLES
-var str = "Hello, playground"  // 'var' esto es una variable
+var str = "Hello, playground"  // 'var' esto es una variable, y puede cambiar.
 str = "Hola Mundo"
 
-let miNombre = "Kontroldev"  // 'let' esto es una constante
+let miNombre = "Kontroldev"  // 'let' esto es una constante y no puede cambiar.
 let miFechaDeNacimiento = "02/01/2024"
 
 
-// DECLARACION MULTIPLES
+// DECLARACION MULTIPLES STRINGs
 let miNombre2 = "kontroldev" ,miFechaDeNacimiento2 = "02/01/2024" ,miLugarDeNacimientio2 = "España"  // Esto tambien se puede hacer cuando vas a declarar constantes/variables con una sola linea de codigo.
+// esto se llamaria concatenacion 👆
 
 
-//TIPO DE DATOS
+// TIPO DE DATOS
 var x = 18  // Int
-
-var y = 18.8 // Float
+var y = 18.8 // Float - Double
+// Float: Es un tipo de 32 bits. Proporciona aproximadamente 6 dígitos de precisión decimal.
+// Double: Es un tipo de 64 bits. Proporciona aproximadamente 15 dígitos de precisión decimal.
 
 
 // OPERADORES ARITMETICOS + - * /
 var sumaEnteros = x + 2
 var sumaFlotantes = y + 2.2
-var suma = Double(x) + y   // aqui debemos deconvertir `x`en tipo Double, ya que y es de tipo Float/Double, y no podemos hacerlo al reves, cambiar y en tipo `Int`por solo cogeria numeros enteros sin decimales.
+var suma = Double(x) + y   // aqui debemos convertir 'X' en tipo Double explicitamente, ya que un numero entero son decimales, por que un 'Int' solo captura los valores son decimales.
 var division = Double(x) / 5
 
 
 // ASIGMACION IMPLICITA Y EXPLICITA
-var x2:Int8 = 18  //asigancion explicita
+var x2:Int8 = 18  //asigancion explicita con ':Int8'
 
-// FLOTANTES
-var y2:Double = 18.8
+var y2:Double = 18.8 // es un Double de manera explicita.
 
+var x3 = 19  // asignacion implicita, Xcode "detecta" de que tipo de datos es la variable creada y la infiere.
