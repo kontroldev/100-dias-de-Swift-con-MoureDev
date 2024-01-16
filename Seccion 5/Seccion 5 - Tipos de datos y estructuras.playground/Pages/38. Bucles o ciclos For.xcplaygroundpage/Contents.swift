13dce4443e4a5f41decfffd518ea9dcb779fe44a
kontroldev <raulgallego79@icloud.com>
2024-01-16 23:26:21 +0100

    Añadido clases diarias. - Día 15✅

--- Seccion 5/Seccion 5 - Tipos de datos y estructuras.playground/Pages/38. Bucles o ciclos For.xcplaygroundpage/Contents.swift
+++ Seccion 5/Seccion 5 - Tipos de datos y estructuras.playground/Pages/38. Bucles o ciclos For.xcplaygroundpage/Contents.swift
@@ -1,3 +1,24 @@
 import Foundation
 
 
+var contadora = 0
+
+/*Un bucle for en Swift es una estructura de control de flujo que se utiliza para repetir un bloque de código un número específico de veces o para iterar sobre elementos de una secuencia (como un rango, un array, o una colección) de manera controlada.*/
+
+for numero in 1...4 {  // esto es un bucle for con operador de rango
+    contadora = contadora + 1
+    print(numero)
+}
+
+var paises = ["MX", "EEUU", "JP"]
+
+for pais in paises { // un bucle for de una 'Array'.
+    print(pais)
+}
+
+
+var paisesDeDiccionario = ["MX", "EEUU", "JP"]
+
+for pais in paisesDeDiccionario { // un bucle for de un 'diccionary'
+    print(pais)
+}
