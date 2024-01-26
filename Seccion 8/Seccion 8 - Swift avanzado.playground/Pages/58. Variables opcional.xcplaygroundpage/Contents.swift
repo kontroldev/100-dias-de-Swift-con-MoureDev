5b92834ebd401cb73177e7bc6a60201421ce7a3c
kontroldev <raulgallego79@icloud.com>
2024-01-26 11:41:12 +0100

    Añadido sección 8 - Día 25 ✅

---
+++ Seccion 8/Seccion 8 - Swift avanzado.playground/Pages/58. Variables opcional.xcplaygroundpage/Contents.swift
@@ -0,0 +1,23 @@
+import Foundation
+
+
+let myStringNumber = "100"
+let myIntNumber = Int(myIntNumber)
+
+let myWrongStringNumber = "Brais"
+let myWrongIntNumber = Int(myWrongStringNumber)  // esto nos devuelve 'nill', al ser un "cadena de texto en vez de un numero, no puede hacer la conversion y nos duvuelve nill.
+
+var myOptionalString: String?
+print(myOptionalString)
+
+
+myOptionalString = "Bienvenidos al curso de Swift avanzado"
+
+if myOptionalString != nill {
+    print("Nuestra variable tiene un valor distinto de nulo")
+} else {
+    print("Nuestra variable es nula")
+}
+
+
+
