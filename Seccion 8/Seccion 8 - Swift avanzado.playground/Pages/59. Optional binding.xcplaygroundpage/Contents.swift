6647844642b94130c5eecc5188d45513a5147bbb
kontroldev <raulgallego79@icloud.com>
2024-01-27 19:20:23 +0100

    actualizado clases - Día 26✅

--- Seccion 8/Seccion 8 - Swift avanzado.playground/Pages/59. Optional binding.xcplaygroundpage/Contents.swift
+++ Seccion 8/Seccion 8 - Swift avanzado.playground/Pages/59. Optional binding.xcplaygroundpage/Contents.swift
@@ -1,4 +1,21 @@
 import UIKit
 
 
+let myStringNumber = "100"
+let myIntNumber = Int(myStringNumber)
+
+let myWrongStringNumber = "Brais"
+let myWrongIntNumber: Int? = Int(myWrongStringNumber)
+
+var myOptionalString: String?
+print(myOptionalString)
+
+myOptionalString = "Bienvenidos al curso de Swift Avanzado"
+
+if myOptionalString != nil {
+    print("Nuestra variable tiene un valor distinto de nulo")
+} else {
+    print("Nuestra variable es nula")
+}
+
 
