5459441eb7a7ae2dae20ac2fbdfdcd45176f3ebd
kontroldev <raulgallego79@icloud.com>
2024-01-30 14:19:10 +0100

    Añadiendo y actualizando clases - Día 29 ✅

--- Seccion 8/Seccion 8 - Swift avanzado.playground/Pages/59. Optional binding.xcplaygroundpage/Contents.swift
+++ Seccion 8/Seccion 8 - Swift avanzado.playground/Pages/59. Optional binding.xcplaygroundpage/Contents.swift
@@ -1,21 +1,33 @@
 import UIKit
 
-// opcional
-let myStringNumber = "100"
-let myIntNumber = Int(myStringNumber)
 
-let myWrongStringNumber = "Brais"
-let myWrongIntNumber: Int? = Int(myWrongStringNumber)
+// enlaces opcionales
+var myOpcionalString: String?
+var myOpcionalString2: String?
+var myOpcionalString3: String?
 
-var myOptionalString: String?
-print(myOptionalString)
 
-myOptionalString = "Bienvenidos al curso de Swift Avanzado"
+myOpcionalString = "Brais"
+myOpcionalString2 = "Moure"
+myOpcionalString3 = "32"
 
-if myOptionalString != nil {
-    print("Nuestra variable tiene un valor distinto de nulo")
+if let myString = myOpcionalString {
+    print(myString)
 } else {
-    print("Nuestra variable es nula")
+    print(myOpcionalString)
 }
 
 
+if let myString = myOpcionalString, let myString2 = myOpcionalString2, let myString3 = myOpcionalString3 {  // aqui enlazamos y evaluamos tres opcionales nuelas en cadena. - ecadenamiento de enlaces opcionales.
+    print("\(myString) \(myString2) \(myString3)")
+} else {
+    print("Alguna variable nuela")
+}
+
+
+// desempaquetado forzado
+print(myOpcionalString!)  // le ponemos una'!' `para hacerlo desempaquetado explicito/forzado.
+
+if myOpcionalString != nil {
+    print(myOpcionalString!)
+}
---
+++ Seccion 8/Seccion 8 - Swift avanzado.playground/Pages/60. Opcional chaining.xcplaygroundpage/Contents.swift
@@ -0,0 +1,32 @@
+import Foundation
+
+
+// Cadenas de opcionales
+class Student {
+    var name: String?
+    var book: Book?
+}
+
+class Book {
+    var pages: Int?
+}
+
+let myStudent = Student()
+
+print(myStudent.name)
+print(myStudent.book?.pages)
+
+myStudent.name = "Brais"
+
+let myBook = Book()
+myBook.pages = 50
+
+myStudent.book = myBook
+
+
+// Enlaces opcional encadenado
+if let pages = myStudent.book?.pages, let name = myStudent.name {
+    print("El libro de \(name) tiene \(pages) paginas")
+} else {
+    print("El libro no tiene paginas")
+}
