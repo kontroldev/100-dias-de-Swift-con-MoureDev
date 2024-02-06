42df5705ccbe446596f94408ea4f7df4387c6be8
KontrolDev <raulgallego79@icloud.com>
2024-02-06 19:28:34 +0100

    Añadido clases - Día 36 ✅

---
+++ Seccion 8/Seccion 8 - Swift avanzado.playground/Pages/66. Protocolos.xcplaygroundpage/Contents.swift
@@ -0,0 +1,38 @@
+import Foundation
+
+
+// Protocolos
+protocol PersonProtocol {
+    var name: String {get set}
+    var age: Int {get set}
+
+    func fullName() -> String
+}
+
+struct Programmer: PersonProtocol {
+    
+    var name: String
+    var age: Int
+    var language: String
+    
+    func fullName() -> String {
+        return "El nombre es \(name), edad: \(age) y su lenguaje favorito es \(language)"
+    }
+}
+
+struct Teacher: PersonProtocol {
+    
+    var name: String
+    var age: Int
+    var subject: String
+    
+    func fullName() -> String {
+        return "El nombre es \(name), edad \(age) e imparte la asignatura de \(subject)"
+    }
+}
+
+let myProgrammer = Programmer(name: "Brais", age: 32, language: "swift")
+let myTeacher = Teacher(name: "Juan", age: 50, subject: "Matematicas")
+
+print(myProgrammer.fullName())
+print(myTeacher.fullName())
