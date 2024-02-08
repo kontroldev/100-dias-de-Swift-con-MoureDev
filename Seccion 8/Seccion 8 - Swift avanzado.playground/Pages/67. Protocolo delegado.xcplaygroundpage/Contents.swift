960f4d43e2bb9183e144daca8f34a08b1b911aa4
KontrolDev <raulgallego79@icloud.com>
2024-02-08 14:01:55 +0100

    Actualizando clase - Día 38 ✅

--- Seccion 8/Seccion 8 - Swift avanzado.playground/Pages/67. Protocolo delegado.xcplaygroundpage/Contents.swift
+++ Seccion 8/Seccion 8 - Swift avanzado.playground/Pages/67. Protocolo delegado.xcplaygroundpage/Contents.swift
@@ -1,44 +1,6 @@
 import Foundation
 
 
-// Clase anterior 66.
-protocol PersonProtocol {
-    var name: String {get set}
-    var age: Int {get set}
-
-    func fullName() -> String
-}
-
-struct Programmer: PersonProtocol {
-    
-    var name: String
-    var age: Int
-    var language: String
-    
-    func fullName() -> String {
-        return "El nombre es \(name), edad: \(age) y su lenguaje favorito es \(language)"
-    }
-}
-
-struct Teacher: PersonProtocol {
-    
-    var name: String
-    var age: Int
-    var subject: String
-    
-    func fullName() -> String {
-        return "El nombre es \(name), edad \(age) e imparte la asignatura de \(subject)"
-    }
-}
-
-let myProgrammer = Programmer(name: "Brais", age: 32, language: "swift")
-let myTeacher = Teacher(name: "Juan", age: 50, subject: "Matematicas")
-
-print(myProgrammer.fullName())
-print(myTeacher.fullName()
-
-
-
 // Protocolos delegados
 class FirstClass: SecondClassProtocol {
     
@@ -47,21 +9,22 @@ class FirstClass: SecondClassProtocol {
         secondClass.delegate = self
         secondClass.callFirst()
     }
+    
     func call() {
         print("Estoy de vuelta!")
     }
 }
-
-    Protocol SecondClassProtocolo {
+protocol SecondClassProtocol {
         func call()
 }
 class SecondClass {
+    
     var delegate: SecondClassProtocol?
+    
     func callFirst() {
-        sleep(5000)
-        
-        delegate?.call()
+    sleep(5)
+    delegate?.call()
+    }
 }
-
-let firstClass = firstClass()
+let firstClass = FirstClass()
 firstClass.callSecond()
