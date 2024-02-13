de92a3af7a616e3d73940225813764d86530078f
KontrolDev <raulgallego79@icloud.com>
2024-02-13 12:08:56 +0100

    Actualización clases - Día 42 ✅

--- Seccion 9/Swift avanzado.playground/Pages/76. Sets.xcplaygroundpage/Contents.swift
+++ Seccion 9/Swift avanzado.playground/Pages/76. Sets.xcplaygroundpage/Contents.swift
@@ -2,5 +2,27 @@ import Foundation
 
 
 // Conjuntos - SETS
-let mySet = Set<String>()
-let mySet2 = Set<String> = ["Brais", "Moure", "32", "Brais"]
+var mySet = Set<String>()
+let mySet2: Set<String> = ["Brais", "Moure", "32", "Brais"]
+let mySet3: Set = ["Brais", "Moure", "32", "Brais"]
+
+
+// Insercion de uno en uno
+mySet.insert("Brais")
+mySet.insert("Moure")
+mySet.insert("32")
+mySet.insert("Brais")
+print(mySet)
+
+
+// Acceso
+if mySet.contains("MoureDev") {
+    print("Existe")
+} else {
+    print("No existe")
+}
+
+
+// Modificacion
+mySet.remove("32")
+print(mySet)
