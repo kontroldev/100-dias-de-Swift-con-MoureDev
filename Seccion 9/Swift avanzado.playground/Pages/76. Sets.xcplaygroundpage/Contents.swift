fc0c87bc46e18af4e3ed3fcd4813bba1d8dfab5c
KontrolDev <raulgallego79@icloud.com>
2024-02-14 10:42:54 +0100

    añadido clases 76 - Día 44 ✅

--- Seccion 9/Swift avanzado.playground/Pages/76. Sets.xcplaygroundpage/Contents.swift
+++ Seccion 9/Swift avanzado.playground/Pages/76. Sets.xcplaygroundpage/Contents.swift
@@ -26,3 +26,38 @@ if mySet.contains("MoureDev") {
 // Modificacion
 mySet.remove("32")
 print(mySet)
+
+
+// Acceso y modificacion por indice
+if let index = mySet.firstIndex(of: "Brais") {
+    mySet.remove(at: index)
+}
+
+print(mySet)
+
+
+// Iteracion
+mySet.insert("32")
+mySet.insert("Brais")
+mySet.insert("Juan")
+mySet.insert("Bienvenidos al curso de Swift")
+
+for myElement in mySet {
+    print(myElement)
+}
+
+// - Opciones de conjuntos o Sets
+let myIntSet: Set = [1, 2, 3, 4, 5]
+let myIntSet2: Set = [0, 2, 3, 6, 7]
+
+// Interseccion
+print(myIntSet.intersection(myIntSet2))
+
+// - Diferencia simetrica
+print(myIntSet.symmetricDifference(myIntSet2))
+
+// Unión
+print(myIntSet.union(myIntSet2))
+
+// Sustraccion
+print(myIntSet.subtracting(myIntSet2))
