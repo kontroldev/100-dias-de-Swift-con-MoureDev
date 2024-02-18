9fde429077d2272e9bd9ebf75cff440526bed021
KontrolDev <raulgallego79@icloud.com>
2024-02-18 18:33:07 +0100

    Actualización clases - Día 48 ✅

--- Seccion 9/Swift avanzado.playground/Pages/78. Sorted.xcplaygroundpage/Contents.swift
+++ Seccion 9/Swift avanzado.playground/Pages/78. Sorted.xcplaygroundpage/Contents.swift
@@ -19,16 +19,24 @@ let mySet: Set = [5,8,1,0,3,9,7,2,4,6]
 
 print(myArray)
 myArray.sorted() // nos deja la coleccion como la declaramos(hace una copia) y nos devuelve otra "copia" ya ordenada.
+
 var myArraySorted = myArray.sorted()
 print(myArray)
 print(myArraySorted)
 
-myArraySorted = myArray.sorted() { (intA, intB) -> Bool in
+myArraySorted = myArray.sorted { (intA, intB) -> Bool in
     return intA > intB
 }
-
 print(myArraySorted)
 
-myDiccionary.sorted { (elementA, elementB) -> Bool in
-    return elementA.key < elementB.key     // Repasar esta seccion
+
+let mySortedDiccionary = myDiccionary.sorted { (elementA, elementB) -> Bool in
+    return elementA.key < elementB.key                           // Repasar esta seccion
 }
+print(mySortedDiccionary)
+
+
+var mySortedSet = mySet.sorted { (intA, intB) -> Bool in
+    return intA > intB
+}
+print(mySortedSet)
