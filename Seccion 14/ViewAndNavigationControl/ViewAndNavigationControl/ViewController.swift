77da52e15e88b7e23f2918497b71250b7c6719cf
kontroldev <raulgallego79@icloud.com>
2024-03-22 09:27:11 +0100

    Sección 14, clase 113 - Día 81 ✅

---
+++ Seccion 14/ViewAndNavigationControl/ViewAndNavigationControl/ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  ViewAndNavigationControl
+//
+//  Created by Raúl Gallego Alonso on 22/3/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var myView: UIView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
