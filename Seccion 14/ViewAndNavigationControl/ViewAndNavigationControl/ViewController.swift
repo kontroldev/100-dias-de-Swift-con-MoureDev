1aeb7aec374d4637c7d48434811554be22913f18
KontrolDev <raulgallego79@icloud.com>
2024-03-23 21:17:26 +0100

    lección 114 ImageViews - Día 82

--- Seccion 14/ViewAndNavigationControl/ViewAndNavigationControl/ViewController.swift
+++ Seccion 14/ViewAndNavigationControl/ViewAndNavigationControl/ViewController.swift
@@ -10,6 +10,8 @@ import UIKit
 class ViewController: UIViewController {
 
     @IBOutlet weak var myView: UIView!
+    @IBOutlet weak var myImage: UIImageView!
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
