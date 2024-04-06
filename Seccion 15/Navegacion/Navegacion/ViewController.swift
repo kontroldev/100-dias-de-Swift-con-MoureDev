056657edfba8e201fb969f05c5c4b6dd3c4294cd
kontroldev <raulgallego79@icloud.com>
2024-04-06 12:38:57 +0200

    Clase 129 - Día 96 ✅

--- Seccion 15/Navegacion/Navegacion/ViewController.swift
+++ Seccion 15/Navegacion/Navegacion/ViewController.swift
@@ -8,21 +8,26 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
- 
+        
     }
-
+    
     @IBAction func botonNegro(_ sender: Any) {
+        print("Soy el boton negro")
     }
     
     @IBAction func botonRosa(_ sender: Any) {
+        printContent("Soy el boton rosa")
     }
     
-    @IBOutlet var botonMarron: UIView!
+    @IBAction func botonmarron(_ sender: Any) {
+        
+        print("Soy el boton marron")
+        
+        
+    }
     
     
 }
-
-
