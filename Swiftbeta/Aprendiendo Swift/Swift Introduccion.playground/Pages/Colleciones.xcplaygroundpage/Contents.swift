e151f1fcdf120055489e5d3008b06c208f625073
kontroldev <raulgallego79@icloud.com>
2024-04-09 05:57:19 +0200

    Primer commit de SwiftBeta.

---
+++ Swiftbeta/Aprendiendo Swift/Swift Introduccion.playground/Pages/Colleciones.xcplaygroundpage/Contents.swift
@@ -0,0 +1,29 @@
+//: [Previous](@previous)
+
+import Foundation
+
+//MARK: -Array
+var arrayString: [String]  // declaramos una array de tipo 'string', tambien podemos hacerlo de la iguiente manera. 👇
+// var arrayString: Array<String>
+
+
+var someInt = [INt]()  // Una Array con valores vacios.
+//var someInt: [Int] = []
+
+var arrayString2 = ["Swift", "Beta"]  // inicializando una 'Array'
+var arrayOfString = Array.init(arrayLiteral: "Swift", "Beta")
+
+
+var arrayString3 = ["1", "2", "3"]
+
+print("The arrayString contains \()")
+
+
+
+
+
+
+
+
+
+//: [Next](@next)
